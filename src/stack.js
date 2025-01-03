44385f723d83a7dc551ed49b17b5807ac065a834
Valentine Tur <valentine.tur.it@gmail.com>
2025-01-03 21:21:23 +0100

    feat: add stack task

--- src/stack.js
+++ src/stack.js
@@ -13,19 +13,21 @@ const { NotImplementedError } = require('../extensions/index.js');
  *
  */
 class Stack {
-  push(/* element */) {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+
+  constructor() {
+    this.items = [];
+  }
+
+  push( element ) {
+    return this.items.push(element);
   }
 
   pop() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+    return this.items.pop();
   }
 
   peek() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+    return this.items.at(-1);
   }
 }
 
